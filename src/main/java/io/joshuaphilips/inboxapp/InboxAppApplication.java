2c12a265e0e0ce4ca4eafe53b41bc5486be19b96
joshuaphilips <philipsjoshua96@gmail.com>
2022-03-04 22:34:58 +0000

    folder table, repository & homepage controller

--- src/main/java/io/joshuaphilips/inboxapp/InboxAppApplication.java
+++ src/main/java/io/joshuaphilips/inboxapp/InboxAppApplication.java
@@ -5,11 +5,9 @@ import java.nio.file.Path;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-@EnableConfigurationProperties(DataStaxAstraProperties.class)
 public class InboxAppApplication {
 
 	public static void main(String[] args) {
