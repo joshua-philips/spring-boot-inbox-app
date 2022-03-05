60ee988316691986a833d2d5823f079daa55b32b
joshuaphilips <philipsjoshua96@gmail.com>
2022-03-05 13:50:57 +0000

    added folder view to inbox page

--- src/main/java/io/joshuaphilips/inboxapp/controllers/InboxController.java
+++ src/main/java/io/joshuaphilips/inboxapp/controllers/InboxController.java
@@ -1,20 +1,45 @@
 package io.joshuaphilips.inboxapp.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import io.joshuaphilips.inboxapp.folders.Folder;
+import io.joshuaphilips.inboxapp.folders.FolderRepository;
 
 @Controller
 public class InboxController {
 
-	@GetMapping("/")
-	public String homePage(@AuthenticationPrincipal OAuth2User principal) {
+	@Autowired
+	private FolderRepository folderRepository;
 
-		if (principal != null && StringUtils.hasText(principal.getAttribute("name"))) {
+	@GetMapping("/")
+	public String homePage(@AuthenticationPrincipal OAuth2User principal, Model model) {
+
+		if (principal != null && StringUtils.hasText(principal.getAttribute("login"))) {
+
+			String userId = principal.getAttribute("login");
+
+			System.out.println(principal.getAttributes());
+
+			List<Folder> userFolders = new ArrayList<>();
+			userFolders.add(new Folder(userId, "Inbox", "blue"));
+			userFolders.add(new Folder(userId, "Sent", "green"));
+			userFolders.add(new Folder(userId, "Important", "yellow"));
+
+//			List<Folder> userFolders = folderRepository.findAllById(userId);
+			model.addAttribute("userFolders", userFolders);
+
 			return "inbox-page";
 		}
+
 		return "index";
 	}
 }
--- src/main/java/io/joshuaphilips/inboxapp/folders/Folder.java
+++ src/main/java/io/joshuaphilips/inboxapp/folders/Folder.java
@@ -10,7 +10,7 @@ import org.springframework.data.cassandra.core.mapping.Table;
 public class Folder {
 
 	@PrimaryKeyColumn(name = "user_id", ordinal = 0, type = PrimaryKeyType.PARTITIONED)
-	private String userId;
+	private String id;
 
 	@PrimaryKeyColumn(name = "label", ordinal = 1, type = PrimaryKeyType.CLUSTERED)
 	private String label;
@@ -18,12 +18,21 @@ public class Folder {
 	@CassandraType(type = Name.TEXT)
 	private String color;
 
-	public String getUserId() {
-		return userId;
+	public Folder() {
 	}
 
-	public void setUserId(String userId) {
-		this.userId = userId;
+	public Folder(String id, String label, String color) {
+		this.id = id;
+		this.label = label;
+		this.color = color;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
 	}
 
 	public String getLabel() {
