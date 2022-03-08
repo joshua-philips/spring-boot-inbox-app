8c7441cc9da23e6d00d8c011c490cf1f44848aae
joshuaphilips <philipsjoshua96@gmail.com>
2022-03-08 15:30:36 +0000

    compose message page

--- src/main/java/io/joshuaphilips/inboxapp/controllers/ComposeController.java
+++ src/main/java/io/joshuaphilips/inboxapp/controllers/ComposeController.java
@@ -1,8 +1,8 @@
 package io.joshuaphilips.inboxapp.controllers;
 
+import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -11,32 +11,26 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import io.joshuaphilips.inboxapp.email.Email;
-import io.joshuaphilips.inboxapp.email.EmailRepository;
 import io.joshuaphilips.inboxapp.folders.Folder;
 import io.joshuaphilips.inboxapp.folders.FolderRepository;
 import io.joshuaphilips.inboxapp.folders.FolderService;
 
 @Controller
-public class EmailViewController {
+public class ComposeController {
 	@Autowired
 	private FolderRepository folderRepository;
 
 	@Autowired
 	private FolderService folderService;
 
-	@Autowired
-	private EmailRepository emailRepository;
-
-	@GetMapping("/emails/{id}")
-	public String emailView(@AuthenticationPrincipal OAuth2User principal, Model model, @PathVariable UUID id) {
-
+	@GetMapping(value = "/compose")
+	public String getComposePage(@AuthenticationPrincipal OAuth2User principal, Model model,
+			@RequestParam(required = false) String to) {
 		if (principal == null || !StringUtils.hasText(principal.getAttribute("login"))) {
 			return "index";
 		}
-
 		String userId = principal.getAttribute("login");
 
 		// Fetch folders
@@ -46,17 +40,14 @@ public class EmailViewController {
 		List<Folder> defaultFolders = folderService.fetchDefaultFolders(userId);
 		model.addAttribute("defaultFolders", defaultFolders);
 
-		Optional<Email> optionalEmail = emailRepository.findById(id);
-		if (optionalEmail.isEmpty()) {
-			return "inbox-page";
+		if (StringUtils.hasText(to)) {
+			String[] splitIds = to.split(",");
+			List<String> uniqueToIds = Arrays.asList(splitIds).stream().map(id -> StringUtils.trimWhitespace(id))
+					.filter(id -> StringUtils.hasText(id)).distinct().collect(Collectors.toList());
+
+			model.addAttribute("toIds", String.join(", ", uniqueToIds));
 		}
 
-		Email email = optionalEmail.get();
-		String toIds = String.join(", ", email.getTo());
-		model.addAttribute("email", email);
-		model.addAttribute("toIds", toIds);
-		return "email-page";
-
+		return "compose-page";
 	}
-
 }
